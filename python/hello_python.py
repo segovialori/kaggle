0f200b1da4f6eb01b018f07c7bd0df39070f5fa7
Lori Segovia <segovialori@gmail.com>
2021-09-04 00:01:01 -0500

    kaggle questions

--- python/hello_python.py
+++ python/hello_python.py
@@ -8,3 +8,35 @@ radius = diameter / 2
 # Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
 area = (pi) * (radius ** 2)
 
+########### Setup code - don't touch this part ######################
+# If you're curious, these are examples of lists. We'll talk about 
+# them in depth a few lessons from now. For now, just know that they're
+# yet another type of Python object, like int or float.
+a = [1, 2, 3]
+b = [3, 2, 1]
+q2.store_original_ids()
+######################################################################
+
+# Your code goes here. Swap the values to which a and b refer.
+# If you get stuck, you can always uncomment one or both of the lines in
+# the next cell for a hint, or to peek at the solution.
+
+######################################################################
+a, b = b , a
+
+## 4. 
+# Alice, Bob and Carol have agreed to pool their Halloween candy and split it evenly among themselves.
+# For the sake of their friendship, any candies left over will be smashed. For example, if they collectively
+# bring home 91 candies, they'll take 30 each and smash 1.
+
+# Write an arithmetic expression below to calculate how many candies they must smash for a given haul.
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
+to_smash
