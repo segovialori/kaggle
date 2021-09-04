3240d5aec6574a90e3aef058eb11f348a0855fdf
Lori Segovia <segovialori@gmail.com>
2021-09-03 23:44:30 -0500

    adding kaggle practice

---
+++ python/hello_python.py
@@ -0,0 +1,10 @@
+######## KAGGLE COURSES ########
+#### HELLO, PYTHON ####
+pi = 3.14159 # approximate
+diameter = 3
+
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter / 2
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = (pi) * (radius ** 2)
+
