8bcbc739c5508d9bdf9fd681b1703a7d4d0b40bd
Lori Segovia <segovialori@gmail.com>
2021-09-12 22:51:39 -0500

    added solution

--- python/hello_python.py
+++ python/hello_python.py
@@ -84,3 +84,11 @@ def sign(num):
         return 1
     else:
         return 0
+#2.
+if total_candies == 1:
+    print("Splitting 1 candy")
+else:
+    print("Splitting", total_candies, "candies")
+Here's a slightly more succinct solution using a conditional expression:
+
+print("Splitting", total_candies, "candy" if total_candies == 1 else "candies")
